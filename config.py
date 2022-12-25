86d0637f24fa0a1a4effb4b3edd974c2a56dfb99
higio97 <92028330+higio97@users.noreply.github.com>
2022-12-25 17:44:58 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,17 @@
 import os
 
-api_id = int(os.environ.get("API_ID", "WAJIB_DIISI"))
-api_hash = os.environ.get("API_HASH", "WAJIB_DIISI")
-bot_token = os.environ.get("BOT_TOKEN", "WAJIB_DIISI")
+api_id = int(os.environ.get("API_ID", "#"))
+api_hash = os.environ.get("API_HASH", "#")
+bot_token = os.environ.get("BOT_TOKEN", "#")
 # =========================================================== #
 
-db_url = os.environ.get("DB_URL", "mongodb://localhost:27017")
+db_url = os.environ.get("DB_URL", "#")
 db_name = os.environ.get("DB_NAME", "telegram") #bisa diganti sesuai kebutuhan
 # =========================================================== #
 
-channel_1 = int(os.environ.get("CHANNEL_1", "WAJIB_DIISI"))
-channel_2 = int(os.environ.get("CHANNEL_2", "WAJIB_DIISI")) #untuk group comentar user
-channel_log = int(os.environ.get("CHANNEL_LOG", "WAJIB_DIISI"))
+channel_1 = int(os.environ.get("CHANNEL_1", "#"))
+channel_2 = int(os.environ.get("CHANNEL_2", "#")) #untuk group comentar user
+channel_log = int(os.environ.get("CHANNEL_LOG", "#"))
 # =========================================================== #
 
 id_admin = int(os.environ.get("ID_ADMIN", "WAJIB_DIISI"))
@@ -38,19 +38,19 @@ biaya_bfrent = int(os.environ.get("BIAYA_BFRENT", "30"))
 hastag = os.environ.get("HASTAG", "#AlterGirl #AlterBoy #AlterAsk #AlterFind #AlterSpill #AlterStory").replace(" ", "|").lower()
 # =========================================================== #
 
-pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph/file/ee4e4864bc4ebfb7819be.jpg")
-pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph/file/2562988d57d7f61384f24.jpg")
+pic_boy = os.environ.get("PIC_BOY", "https://telegra.ph/file/8f7b8bdbae40c7af98e7f.png")
+pic_girl = os.environ.get("PIC_GIRL", "https://telegra.ph/file/f4a3f87dd3551fea551ef.png")
 # =========================================================== #
 
-pesan_join = os.environ.get("PESAN_JOIN", "Tidak dapat diakses harap join terlebih dahulu")
-start_msg = os.environ.get("START_MSG", "Hai {fullname} 🌱\n\nIni adalah bot menfess, semua pesan yang kamu kirim akan masuk ke channel secara anonymous. ketik /help")
+pesan_join = os.environ.get("PESAN_JOIN", "Tidak Dapat Diakses Harap Join Terlebih Dahulu")
+start_msg = os.environ.get("START_MSG", "Hai {mention} 🌱\n\n<b>Alter FWB Bot</b> adalah Bot Auto Post, Semua Pesan Yang Kamu Kirim Akan Masuk Ke Channel @AlterFWB1 Secara Anonymous. Untuk Bantuan Ketik /help")
 
 gagalkirim_msg = os.environ.get("GAGAL_KIRIM", """
-{mention}, pesan mu gagal terkirim silahkan gunakan hastag:
+{mention}, Pesan Mu Gagal Terkirim Silahkan Gunakan Hashtag Berikut:
 
-#AlterBoy / #AlterGirl untuk Mencari Pasangan, Teman , Partner FWB
-#AlterAsk untuk Bertanya
-#AlterStory untuk Berbagi Cerita
-#AlterSpill untuk Spill Masalah
-#AlterFind untuk Mencari Pasangan, Teman, Partner FWB
+#AlterBoy / #AlterGirl (Untuk Mencari Pasangan, Teman , Partner FWB)
+#AlterAsk (Untuk Bertanya)
+#AlterStory (Untuk Berbagi Cerita)
+#AlterSpill (Untuk Spill Masalah)
+#AlterFind (Untuk Mencari Pasangan, Teman, Partner FWB)
 """)
